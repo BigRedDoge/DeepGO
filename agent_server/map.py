241ff0571554b7e7189fed3bd4708a6a4e96fbc6
Sean Clifford <seancliff01@gmail.com>
2022-09-04 13:32:05 -0400

    Fixed frame_crop import

--- agent_server/map.py
+++ agent_server/map.py
@@ -1,12 +1,10 @@
-from mmap import MADV_AUTOSYNC
-from frame_crop import FrameCrop
+from ..utils.frame_crop import FrameCrop
 
 class Map:
 
     def __init__(self):
         self.map = None
-        self.map_coords = (0, 0, 0, 0)
-        self.set_map()
+        self.map_coords = (15, 435, 485, 475)
         
     def get_map(self, frame):
         map_crop = FrameCrop(frame, *self.map_coords)
