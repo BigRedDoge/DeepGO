f6e1f111266ccc26e883251e54a7cf4e39a216ac
Sean Clifford <seancliff01@gmail.com>
2022-09-05 13:40:49 -0400

    Changed screen capture from d3dshot to mss and added window size configuration

--- utils/screen_stream.py
+++ utils/screen_stream.py
@@ -1,15 +1,17 @@
-import d3dshot
+#import d3dshot
 import threading
 import time
 import numpy as np
-
+from mss import mss
+from PIL import Image
+import cv2
 
 class ScreenStream():
 
     """
     capture output can be either "numpy" or "pil"
     """
-    def __init__(self, capture_output="numpy"):
+    def __init__(self, window_x=2560, window_y=1440, capture_output="numpy"):
         if capture_output not in ["numpy", "pil"]:
             raise ValueError("capture_output must be either 'numpy' or 'pil'")
         self.capture_output = capture_output
@@ -17,9 +19,12 @@ class ScreenStream():
         self.stop_stream = False
         self.frame_lock = threading.Lock()
 
-        self.frame = d3dshot.create(capture_output=self.capture_output)
+        self.frame = None
         self.stream = None
         self.stream_thread = None
+
+        self.window_x = window_x
+        self.window_y = window_y
 
     def start(self):
         self.stream_thread = threading.Thread(target=self.start_stream)
@@ -27,17 +32,23 @@ class ScreenStream():
         print("Stream started")
 
     def start_stream(self):
-        self.stream = d3dshot.create(capture_output=self.capture_output)
-
+        #self.stream = d3dshot.create(capture_output=self.capture_output)
+        monitor = {"top": 0, "left": 0, "width": self.window_x, "height": self.window_y}
         #with self.frame_lock:
         while not self.stop_stream:
-            self.frame = self.stream.screenshot()
-            if self.frame is None:
-                break
+            with mss() as sct:
+                # 1280 windowed mode for CS:GO, at the top left position of your main screen.
+                # 26 px accounts for title bar. 
+                img = sct.grab(monitor)
+                #create PIL image
+                img = Image.frombytes("RGB", img.size, img.bgra, "raw", "BGRX")
+                #imgarr = np.asarray(img)
+                #imgarr = cv2.cvtColor(imgarr, cv2.COLOR_BGR2RGB)
+                self.frame = img
 
     def stop(self):
         self.stop_stream = True
-        self.stream.stop()
+        #self.stream.stop()
         
         print("Stream stopped")
 
