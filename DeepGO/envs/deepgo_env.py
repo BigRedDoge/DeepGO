a097172af9c871747d9a2cbcaa13771906373967
Sean Clifford <seancliff01@gmail.com>
2022-09-06 09:30:52 -0400

    Added basic step, reset, render, and _get_obs functions

--- DeepGO/envs/deepgo_env.py
+++ DeepGO/envs/deepgo_env.py
@@ -3,6 +3,7 @@ from agent import Agent
 import cv2
 import numpy as np
 from DeepGO.envs.frame_convert import frame
+from DeepGO.envs.reward_calculator import RewardCalculator
 
 
 class DeepGOEnv(gym.Env):
@@ -13,25 +14,39 @@ class DeepGOEnv(gym.Env):
         self.agent = Agent()
         self.agent.start_connection()
 
-        self.input_width = 512
-        self.input_height = 512
+        self.input_width = 256
+        self.input_height = 144
 
-        self.action_space = gym.spaces.Box(low=0.0, high=1.0, shape=(7,), dtype=np.float32)
-        self.observation_space = gym.spaces.Box(low=0, high=255, shape=(self.input_height, self.input_width, 3), dtype=np.uint8)
+        self.action_space = gym.spaces.Box(low=-1.0, high=1.0, shape=(7,), dtype=np.float32)
+        self.observation_space = gym.spaces.Box(low=0, high=255, shape=(self.input_width, self.input_height, 3), dtype=np.uint8)
         
-        self.frame = frame(self.agent.get_state().get_frame())
+        self.frame = self._get_obs()
+        self.reward_calc = RewardCalculator()
+
+        self.agent_state = self.agent.get_state()
+        self.previous_agent_state = self.agent_state
 
     def step(self, action):
         #screen = self.get_frame()
+        self.agent_state = self.agent.get_state()
         screen = self._get_obs()
-        reward = 0
+        
+        reward = self.reward_calc.calculate_reward(self.agent_state, self.previous_agent_state)
+
+        agent_action = self._convert_action(action)
+        #self.agent.send_input(agent_action)
+
         done = False
-        #print(screen, reward, done)
-        return screen, reward, done, {}
+        info = {}
+
+        self.previous_agent_state = self.agent_state
+
+        return screen, reward, done, info
 
     def reset(self):
         #return self.get_frame()
-        return self._get_obs()
+        self.agent_state = self.agent.get_state()
+        return np.zeros((256, 144, 3), dtype=np.uint8) #self._get_obs()
     
     def render(self, mode='human'):
         cv2.imshow('DeepGO', self._get_obs())
@@ -39,3 +54,17 @@ class DeepGOEnv(gym.Env):
         
     def _get_obs(self):
         return frame(self.agent.get_state().get_frame())
+
+    def _convert_action(self, action):
+        converted = []
+        for a in range(5):
+            if action[a] > 0:
+                press = 1
+            else:
+                press = 0
+            converted.append(press)
+            
+        converted.append(action[5])
+        converted.append(action[6])
+
+        return converted
