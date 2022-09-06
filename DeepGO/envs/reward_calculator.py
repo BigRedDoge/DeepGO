afc831899c38e624cbae5b1453212b8896b101d4
Sean Clifford <seancliff01@gmail.com>
2022-09-06 09:32:02 -0400

    RewardCalculator handles calculating reward for current step

--- DeepGO/envs/reward_calculator.py
+++ DeepGO/envs/reward_calculator.py
@@ -6,5 +6,14 @@ class RewardCalculator:
     def __init__(self):
         pass
 
-    def calculate_reward(self, agent_state):
-        pass
+    def calculate_reward(self, state, previous_state):
+        current_state = state.get_game_state()
+        prev_state = previous_state.get_game_state()
+        
+        if None not in [current_state, prev_state]:
+            #print(state.get_game_state().player.match_stats)
+            current_kills = current_state.player.match_stats['kills']
+            prev_kills = prev_state.player.match_stats['kills']
+            if current_kills > prev_kills:
+                return 1
+        return 0
